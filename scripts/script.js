9283f4de10455d91a8315c69c561674c9a394815
sgu6ena <78623661+sgu6ena@users.noreply.github.com>
2021-07-03 20:38:48 +0300

    Login: spiner + enter

--- scripts/script.js
+++ scripts/script.js
@@ -189,39 +189,52 @@ selectPayment.onchange = () => {
 }
 
 
-//* * флип логина
+//* *  логин
 
 const buttonModalLoginNext = document.querySelector('.button-modal-login-next');
 const loginBackward = document.querySelectorAll('.login-backward');
-
 const buttonModalForgottenPassword = document.querySelector('.button-modal-forgotten-password');
 const loginButtonBackward = document.querySelector('.login-button-backward');
+
 const inputLogin = document.querySelector('.input-login');
 const modalLoginTextDescr = document.querySelector('.modal-login-text-descr');
 const modalLoginTextDescrSMS = document.querySelector('.modal-login-text-descr-sms');
-const cardLogin = document.querySelector('.card-login')
-const cardPassword = document.querySelector('.card-pass')
-const cardSMS = document.querySelector('.card-sms')
 
+const cardLogin = document.querySelector('.card-login');
+const cardPassword = document.querySelector('.card-pass');
+const cardSMS = document.querySelector('.card-sms');
+const loginDangerText = document.querySelector('.login-danger-text');
+const spinnerBorder = document.querySelector('.spinner-border');
 
 
 inputLogin.addEventListener('keyup', () => {
-    console.log(inputLogin.value.length);
-    if (inputLogin.value.length<11) {
+
+    if (inputLogin.value.length < 11) {
         buttonModalLoginNext.disabled = true;
-    } else {  buttonModalLoginNext.disabled = false;
-        modalLoginTextDescr.innerHTML = `для номера ${inputLogin.value}`;
-
+    } else if (inputLogin.value.match(/^0{1}\(7{2}\d\)\d{5}$/)!== null) {
+            buttonModalLoginNext.disabled = false; 
+            modalLoginTextDescr.innerHTML = `для номера ${inputLogin.value} `;
+        }
+        else{
+            loginDangerText.style.display ='block';
+        }
     }
-})
+)
 
 
-
-buttonModalLoginNext.addEventListener('click', () => {
-    cardLogin.classList.toggle('hidden');
+buttonModalLoginNext.addEventListener('click', (e) => {
+    e.preventDefault();
+    cardLogin.classList.toggle('deactivate');
+    spinnerBorder.classList.toggle('hidden');
+    setTimeout(()=>{
+        cardLogin.classList.toggle('hidden');
+        spinnerBorder.classList.toggle('hidden');
     cardPassword.classList.toggle('hidden');
+    cardLogin.classList.toggle('deactivate');
     loginButtonBackward.disabled = false;
     buttonModalLoginNext.disabled = true;
+    },3000);
+
 });
 
 
@@ -236,8 +249,7 @@ loginBackward.forEach(item => item.addEventListener('click', () => {
     cardLogin.classList.remove('hidden');
     cardPassword.classList.add('hidden');
     cardSMS.classList.add('hidden');
-    modalLoginTextDescrSMS.innerHTML =  `для номера ${inputLogin.value} `;
+    modalLoginTextDescrSMS.innerHTML = `для номера ${inputLogin.value} `;
     loginButtonBackward.disabled = true;
     buttonModalLoginNext.disabled = false;
 }));
-
