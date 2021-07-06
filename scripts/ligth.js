761c20d1483f8469e9a581be28b8b06254a1d125
Natalja Issar <78623661+sgu6ena@users.noreply.github.com>
2021-07-06 12:28:28 +0300

    light.html

--- scripts/ligth.js
+++ scripts/ligth.js
@@ -1,15 +1,15 @@
 const swiper = new Swiper(".mySwiper", {
-    speed:500,
+    speed:700,
     direction: "vertical",
     slidesPerView: 1,
     spaceBetween: 0,
     mousewheel: true,
     navigation: {
         nextEl: '.big-light-next-slide',
-        // prevEl: '.big-light-next-slide',
     },
     pagination: {
         el: ".swiper-pagination",
         clickable: true,
     },
+    effect:'fade',
 });
