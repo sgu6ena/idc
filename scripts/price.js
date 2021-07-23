69eacc1fec5638ae000b67aa4723afaa3bdc52d0
Natalja Issar <78623661+sgu6ena@users.noreply.github.com>
2021-07-23 16:49:46 +0300

    tracer

--- scripts/price.js
+++ scripts/price.js
@@ -4,11 +4,11 @@ const prices = document.querySelectorAll('.price');
 prices.forEach(price=>price.addEventListener('mouseover',()=>{
 const priceCount = price.querySelector('.price-count');
 priceCount.innerHTML = priceCount.textContent/dollar;
-price.querySelector('.price-currency').innerHTML=" $";
+price.querySelector('.price-currency').innerHTML=" USD";
 }));
 
 prices.forEach(price=>price.addEventListener('mouseout',()=>{
     const priceCount = price.querySelector('.price-count');
     priceCount.innerHTML = priceCount.textContent*dollar;
-    price.querySelector('.price-currency').innerHTML=" руб.";
+    price.querySelector('.price-currency').innerHTML=" RUP";
     }));
